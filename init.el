a800c53968c275ab3ac35eb3761dce4459069e2d
Chris Cummins <chrisc.101@gmail.com>
2013-04-11 18:54:29 +0100

    Fix C-M-s and C-M-r bindings

--- init.el
+++ init.el
@@ -304,7 +304,7 @@
 (global-set-key (kbd "C-s")   'isearch-forward-regexp)
 (global-set-key (kbd "C-r")   'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-s") 'isearch-backward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
 
 ;; Turn off visual line wrap.
 (setq default-truncate-lines 1)
