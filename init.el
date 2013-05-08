a97c1fbd4fa032ed32881a583ec0f5d2f31811cd
Chris Cummins <chrisc.101@gmail.com>
2013-05-08 12:15:31 +0100

    Ensure cl-macs are available before use

--- init.el
+++ init.el
@@ -91,6 +91,8 @@
             kill-emacs-query-functions))
 
 ;; Don't prompt to kill active processes on close.
+(require 'cl-macs)
+
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
   "Prevent annoying \"Active processes exist\" query when you quit Emacs."
   (cl-flet ((process-list ())) ad-do-it))
